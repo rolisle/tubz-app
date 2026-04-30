62a2404143e12d28970b4262eba49c3119b4daca
rolisle <robbie.lisle@live.com>
2026-04-30 09:36:44 +0100

    fix padding

--- app/(tabs)/stock.tsx
+++ app/(tabs)/stock.tsx
@@ -786,7 +786,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    paddingHorizontal: 20,
     paddingTop: 16,
     paddingBottom: 12,
   },
